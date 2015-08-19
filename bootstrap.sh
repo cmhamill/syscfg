e6f40fab17d9942fbef6eefe88751d036c5e8503
C. Morgan Hamill <me@cmhamill.org>
2015-08-19 16:10:46 -0700

    [bootstrap.sh] Add usage info and argument parsing.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
+source lib/lib.sh
+
 PATH="$(dirname "$(readlink -f "$0")")/bin:$PATH"
+
+PROG="bootstrap.sh"
+USAGE="Usage: $PROG [-h]
+
+Bootstrap a new workstation.
+
+    -h, --help  print this help message
+
+Assumptions about workstation state:
+- system has just come out of a debian-installer run
+- system is the current Debian stable distribution
+- /boot is on an encrypted partition
+"
+
+ARGS=$(getopt -o h --long help -n "$PROG" -- "$@")
+[ $? == 0 ] || print_usage
+eval set -- "$ARGS"
+
+while true; do
+    case "$1" in
+        -h|--help)  print_help;;
+        --)         shift; break;;
+        *)          err $EX_SOFTWARE "internal error!";;
+    esac
+done
